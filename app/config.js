963b7c5bfb762def0d740b4a84fd0c0304df4390
razzin <mastertogo@vp.pl>
2021-05-14 16:22:14 +0200

    Lab09

--- app/config.js
+++ app/config.js
@@ -1,6 +1,6 @@
 const config = {
   port: process.env.PORT || 3002,
-  databaseUrl: process.env.MONGODB_URI || 'baza danych',
+  databaseUrl: "mongodb+srv://pwsz-user:pwsz-pass@cluster0.thbeg.mongodb.net/pwsz-db?retryWrites=true&w=majority" || 'baza danych',
   JwtSecret: process.env.JWT_SECRET || 'secret'
 };
 
